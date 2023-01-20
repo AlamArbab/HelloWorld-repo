58e9723e813c372f4cb64c0f640e01ebd24a0ba6
AlamArbab <122641961+AlamArbab@users.noreply.github.com>
2023-01-20 14:46:19 -0600

    Add files via upload

---
+++ convertingseconds.py
@@ -0,0 +1,20 @@
+def convert_seconds(seconds):
+    hours = seconds // 3600
+    minutes = (seconds % 3600) // 60
+    remaining_seconds = seconds % 60
+    return hours, minutes, remaining_seconds
+
+input_seconds = int(input("Enter the number of seconds between 0 and 86,399: "))
+hours, minutes, seconds = convert_seconds(input_seconds)
+print(f"{hours} hours, {minutes} minutes, {seconds} seconds")
+ 
+
+
+
+
+
+
+
+
+
+	
